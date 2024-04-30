88ae386f89ae3bb9f3caa578a3b59f0a75e81040
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-04-30 20:37:45 +0600

    all crafts page responsove done

--- src/components/Pages/ArtsAndCrafts/AllArtsCrafts.jsx
+++ src/components/Pages/ArtsAndCrafts/AllArtsCrafts.jsx
@@ -11,7 +11,7 @@ const AllArtsCrafts = () => {
         <div>
             <Navbar></Navbar>
 
-            <div className="container mx-auto">
+            <div className="container mx-auto px-3 lg:px-0">
                 <JackInTheBox>
                     <h2 className="text-center font-rubic font-bold text-2xl mt-5">Here is our all the collections</h2>
                 </JackInTheBox>
@@ -19,7 +19,7 @@ const AllArtsCrafts = () => {
                 <div>
                     <div className="my-10">
                         {/* Craft Item Cards */}
-                        <div className="grid grid-cols-2 gap-6">
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
                             {
                                 allCraftItems.map(singleCraftItem => <CraftItemCard key={singleCraftItem._id} singleCraftItem={singleCraftItem}></CraftItemCard>)
                             }
