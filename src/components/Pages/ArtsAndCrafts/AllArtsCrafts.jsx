52650e86657fb450ee74c0c05d3f94cdf6e4d988
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-06-25 23:33:48 +0600

    added sorting option

--- src/components/Pages/ArtsAndCrafts/AllArtsCrafts.jsx
+++ src/components/Pages/ArtsAndCrafts/AllArtsCrafts.jsx
@@ -4,17 +4,38 @@ import Navbar from "../Shared/Navbar";
 import CraftItemCard from "./CraftItemCard";
 import { JackInTheBox } from "react-awesome-reveal";
 import { AuthContext } from "../../Providers/AuthProvider";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 const AllArtsCrafts = () => {
     const allCraftItems = useLoaderData();
+    const [allSubCategories, setAllSubCategories] = useState([]);
+    const [selectedSubCategory, setSelectedSubCategory] = useState('Show All');
+    const [sortedItems, setSortedItems] = useState([]);
 
     const { loading } = useContext(AuthContext);
+
+    useEffect(() => {
+        fetch(`https://clay-and-co-server.vercel.app/subCategory`)
+            .then(res => res.json())
+            .then(data => {
+                setAllSubCategories(data);
+            })
+    }, [])
+
+
 
     if (loading) {
         return <div className="flex justify-center mt-40">
             <span className="loading loading-spinner loading-lg"></span>
         </div>
+    }
+
+    const handleSort = e => {
+        const selected = e.target.value;
+        setSelectedSubCategory(selected);
+        const searchedItems = allCraftItems.filter(someItems => someItems.subCategory === selected
+        );
+        setSortedItems(searchedItems);
     }
 
     return (
@@ -27,11 +48,24 @@ const AllArtsCrafts = () => {
                 </JackInTheBox>
 
                 <div>
+                    <div className="mt-5 text-center flex justify-center items-center gap-3">
+                        <h2 className="text-lg font-rubic">Sort By:</h2>
+                        <select onChange={handleSort} className="select border-primaryColor w-full max-w-xs font-rubic">
+                            <option defaultValue>Show All</option>
+                            {
+                                allSubCategories.map((subCategory, idx) => <option key={idx}>{subCategory.subCategory}</option>)
+                            }
+                        </select>
+                    </div>
+
                     <div className="my-10">
                         {/* Craft Item Cards */}
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
-                            {
+                            {selectedSubCategory === 'Show All' ?
                                 allCraftItems.map(singleCraftItem => <CraftItemCard key={singleCraftItem._id} singleCraftItem={singleCraftItem}></CraftItemCard>)
+                                :
+
+                                sortedItems.map(singleSortedItems => <CraftItemCard key={singleSortedItems._id} singleCraftItem={singleSortedItems}></CraftItemCard>)
                             }
                         </div>
                     </div>
